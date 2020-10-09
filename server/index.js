9ddb75d6e8c77d728d6c61ad8c03e9924a228216
Kubilay-web <69966389+Kubilay-web@users.noreply.github.com>
2020-10-09 20:49:34 +0300

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,35 @@
+  
+var path = require('path')
+const express = require('express')
+const mockAPIResponse = require('./mockAPI.js')
+
+var AYLIENTextAPI = require('aylien_textapi');
+var textapi = new AYLIENTextAPI({
+   
+    application_key: process.env.API_KEY
+});
+
+
+const dotenv = require('dotenv');
+dotenv.config();
+
+const app = express()
+
+app.use(express.static('dist'))
+
+console.log(__dirname)
+
+app.get('/', function (req, res) {
+    res.sendFile('dist/index.html')
+})
+
+// designates what port the app will listen to for incoming requests
+app.listen(8080, function () {
+    console.log('Example app listening on port 8080!')
+})
+
+app.get('/test', function (req, res) {
+    res.send(mockAPIResponse)
+})
+
+console.log(`Your API key is ${process.env.API_KEY}`);
