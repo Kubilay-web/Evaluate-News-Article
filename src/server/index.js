c556aeac61ba1d4f66a1a7f07bb23448e169e41a
Kubilay Özdemir <69966389+Kubilay-web@users.noreply.github.com>
2020-11-16 13:05:47 +0300

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -5,13 +5,11 @@ var path = require('path')
 
 
 const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+
 const app = express();
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { read } = require('fs');
-const { response } = require('express');
-const { ValidationError } = require('webpack');
+
 
 
 
@@ -20,33 +18,46 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 console.log(__dirname)
+
+
 //display of UI
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
     //res.sendFile(path.resolve('src/client/views/index.html'))
 })
 // designates what port the app will listen to for incoming requests
-app.listen(8083, function () {
-    console.log('Example app listening on port 8083!')
+app.listen(8888, function () {
+    console.log('Example app listening on port 8888!')
 })
     
-let baseUrl = 'https://api.meaningcloud.com/sentiment-2.1?key=';
-let API_KEY = process.env.API_KEY;
-console.log(`API key: ${API_KEY}`)
-//POST request
-app.post('/test', async (req, res) => {
-    //if (Validation.isURL(req.body.input)) {
-    //    reqType = 'url';
-    //} 
-    const response = await fetch (`${baseUrl}${API_KEY}&of=json&txt&lang=en&url=${req.body}`);
-    console.log('server response: ', response)
-    const data = await response.json();
-    console.log('server side: ' , data)
-    const projectData = {
-        score_tag: data.score_tag,
-        confidence: data.confidence,
-        irony: data.irony,
-        subjectivity: data.subjectivity,
-    }
+
+projectData = {};
+
+app.get('/all', function (req, res) {
+    console.log("Get request OK.");
     res.send(projectData);
+    console.log(projectData);
 })
+
+
+//POST Route
+
+
+
+app.post('/addText', addData)
+
+function addData(request, response) {
+
+    projectData.Text = request.body.userResponse;
+    projectData.inip = request.body.Inıp;
+    projectData.endp = request.body.Endp;
+    projectData.bop = request.body.Bop;
+    projectData.score_tag = request.body.ScoreTag;
+    projectData.agreement = request.body.Agreement;
+
+    
+    
+    
+    response.end(projectData);
+    console.log(projectData);
+}
