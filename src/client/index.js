98e9e79901e3865694cadfe4d1b60b334d4faade
Kubilay Özdemir <69966389+Kubilay-web@users.noreply.github.com>
2020-11-16 13:04:22 +0300

    Update index.js

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,6 @@
-import { checkForName } from './js/nameChecker'
-import { urlChecker } from './js/urlChecker'
-import { handleSubmit } from './js/formHandler'
 
+
+import { performAction } from './js/formHandler'
 
 
 import './styles/resets.scss'
@@ -9,10 +8,7 @@ import './styles/base.scss'
 import './styles/form.scss'
 import './styles/header.scss'
 
-
-
-console.log(checkForName);
-console.log(handleSubmit);
-console.log(urlChecker);
-console.log("CHANGE!!");
-export { checkForName,handleSubmit, urlChecker}
+export {
+   
+   performAction
+  }
